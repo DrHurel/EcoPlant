cafa20446e65eb913e85dde52cf23fd15ec24b6d
Hurel <jhurel@free-mobile.fr>
2025-04-08 15:27:33 +0200

    Initial commit

---
+++ app/src/main/java/fr/hureljeremy/gitea/ecoplant/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package fr.hureljeremy.gitea.ecoplant.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val bordoColor = Color(0xFF6E0D25)
+val oliveColor = Color(0xFFBBCEA8)
+val beigeColor = Color(0xFFE3D87E)
+val yellowColor = Color(0xFFF0EC57)
+val brownColor = Color(0xFF774E24)
