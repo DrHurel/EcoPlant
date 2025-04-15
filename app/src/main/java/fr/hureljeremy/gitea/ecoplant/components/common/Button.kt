e3b4087c704c4871fca7af3a1dbb2d484fc2f766
Hurel <jhurel@free-mobile.fr>
2025-04-15 14:42:40 +0200

    Add AuthService for user authentication; implement login, logout, registration, and token management methods

--- app/src/main/java/fr/hureljeremy/gitea/ecoplant/components/common/Button.kt
+++ app/src/main/java/fr/hureljeremy/gitea/ecoplant/components/common/Button.kt
@@ -35,7 +35,7 @@ class Button : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.components.fragment_button, container, false)
+        return inflater.inflate(R.layout.fragment_button, container, false)
     }
 
     companion object {
