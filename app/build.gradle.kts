b2f00191541a56c471dac9b7a813fbc389f5b557
Vincent <flyber11@gmail.com>
2025-04-22 17:09:44 +0200

    Scanner page done + add dependencies + add uses-feature and uses-permission for the camera

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,10 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
 
+    //dep camera
+    implementation("androidx.camera:camera-camera2:1.3.1")
+    implementation("androidx.camera:camera-lifecycle:1.3.1")
+    implementation("androidx.camera:camera-view:1.3.1")
+
+
 }
--- app/src/main/java/fr/hureljeremy/gitea/ecoplant/MainActivity.kt
+++ app/src/main/java/fr/hureljeremy/gitea/ecoplant/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.home_page)
+        setContentView(R.layout.scanner_page)
     }
 }
 
